8b681104665b2f8b73f0e596e1a8918c67106951
galasanchez <gala.sanchez@alumno.buap.mx>
2024-04-29 15:20:02 -0600

    Tarea 4 a)

--- Tarea 4/Red densa secuencial con Keras.py
+++ Tarea 4/Red densa secuencial con Keras.py
@@ -12,8 +12,8 @@ from tensorflow.keras.utils import to_categorical
 
 #Configuración de learning rate, número de épocas y batch size
 learning_rate = 0.01
-epochs = 40
-batch_size = 15
+epochs = 30
+batch_size = 10
 
 
 #Cargar los datos del conjunto MNIST
